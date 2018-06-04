7b0cb88b291ae9971f00e5e6b97eae8f384daddd
José Luis  Mejía Vargas <joseluismejiavargas@MacBook-Pro-de-Jose.local>
2018-06-04 17:49:34 -0500

    test_jwt_

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,11 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
---
+++ src/Controller/ApiController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class ApiController
+ *
+ * @Route("/api")
+ */
+
+class ApiController extends Controller
+{
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction() {
+    	print_r('hola');
+    }
+}
